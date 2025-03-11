e44c9820e3479b15e76696442bbbffc63b6551aa
Thanh Nguyen <thanhnguyen.rynt@gmail.com>
2025-03-11 15:48:45 +0700

    bỏ folder source

--- src/routes/nguyenVatLieuRoutes/nguyenVatLieuRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const nguyenVatLieuController = require('../../controller/nguyenVatLieuController/nguyenVatLieuController');
-
-const router = express.Router();
-
-router.use('/nguyen-vat-lieu', nguyenVatLieuController);
-
-module.exports = router;
