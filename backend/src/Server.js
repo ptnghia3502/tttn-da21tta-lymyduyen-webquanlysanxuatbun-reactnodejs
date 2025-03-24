cb4bd072f315b413b5ba1a8c1f48289f4d363a60
Trong Nghia <ptnghia3502@gmail.com>
2025-03-24 10:54:28 +0700

    Fix cors

--- backend/src/Server.js
+++ backend/src/Server.js
@@ -19,7 +19,7 @@ app.use(helmet());
 
 // Middleware
 app.use(cors({
-  origin: ["http://localhost:3000", "http://localhost:3001", "https://quanly-sanxuat-tts-vnpt.vercel.app/"],
+  origin: ["http://localhost:3000", "http://localhost:3001", "https://quanly-sanxuat-tts-vnpt.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
